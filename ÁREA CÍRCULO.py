b84eb55040ce4ffef89ad63c167ae0253313b4c0
yaelinares <yaelprivado@gmail.com>
2023-09-21 21:58:12 +0200

    área círculo

---
+++ ÁREA CÍRCULO.py
@@ -0,0 +1,6 @@
+#ÁREA CÍRCULO
+print("Área círculo")
+radio = int(input("introduce radio: "))
+pi = 3.14
+resultado= radio**2*pi
+print(f"El área de un círculo de radio {radio} es {resultado} metros cuadrados")
